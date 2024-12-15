7768360993ff5402858d0b7a5b52890cbda2ea69
Bumper <61375058+EmilDimov93@users.noreply.github.com>
2024-12-15 15:16:09 +0100

    Commit

--- obj/Debug/net8.0/Football.AssemblyInfo.cs
+++ obj/Debug/net8.0/Football.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Football")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1dbe79cf4b44fc38bdb36928ef28b815ed9e4f1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b035f28a2ccda71af93983e135675f1ed2bfadb7")]
 [assembly: System.Reflection.AssemblyProductAttribute("Football")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Football")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
